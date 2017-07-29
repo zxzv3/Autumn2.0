fe9399c6d3303a2ef96ddf87ce331935b2dad957
tocurd <tocurd@.com>
2017-07-29 08:12:13 +0800

    test

--- application/logs/log-2017-07-29.php
+++ application/logs/log-2017-07-29.php
@@ -22,3 +22,117 @@ ERROR - 2017-07-29 12:36:55 --> Severity: Notice --> Undefined index: PATH_INFO 
 ERROR - 2017-07-29 12:48:41 --> Severity: Parsing Error --> syntax error, unexpected '_' (T_STRING), expecting ')' /home/tocurd/www/application/controllers/admin/user/Groups.php 54
 ERROR - 2017-07-29 12:51:40 --> 404 Page Not Found: /index
 ERROR - 2017-07-29 12:51:53 --> 404 Page Not Found: /index
+ERROR - 2017-07-29 08:03:25 --> 404 Page Not Found: /index
+ERROR - 2017-07-29 08:03:41 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): ����Ŀ����������ܾ����޷����ӡ�
+ F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:03:41 --> Unable to connect to the database
+ERROR - 2017-07-29 08:03:43 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): ����Ŀ����������ܾ����޷����ӡ�
+ F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:03:43 --> Unable to connect to the database
+ERROR - 2017-07-29 08:05:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'autumn2.0' F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:05:50 --> Unable to connect to the database
+ERROR - 2017-07-29 08:06:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:06:01 --> Unable to connect to the database
+ERROR - 2017-07-29 08:06:07 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'autumn2.0' F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:06:07 --> Unable to connect to the database
+ERROR - 2017-07-29 08:07:09 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:07:09 --> Unable to connect to the database
+ERROR - 2017-07-29 08:07:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) F:\xampp\htdocs\cloud_b2b\application\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2017-07-29 08:07:50 --> Unable to connect to the database
+ERROR - 2017-07-29 08:08:02 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:03 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:04 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:05 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:06 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:07 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:08 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:09 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:10 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:11 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:12 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:13 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:14 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:15 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:16 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:17 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:18 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:19 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:20 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:21 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:22 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:23 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:24 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:25 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:26 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:27 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:28 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:29 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:30 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:31 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:32 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:33 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:34 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:35 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:36 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:37 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:38 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:39 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:40 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:41 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:42 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:43 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:44 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:45 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:46 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:47 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:48 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:49 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:50 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:51 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:52 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:53 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:54 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:55 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:56 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:57 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:58 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:08:59 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:00 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:01 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:02 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:03 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:04 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:05 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:06 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:07 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:08 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:09 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:10 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:11 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:12 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:13 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:14 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:15 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:16 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:17 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:18 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:19 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:20 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:21 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:22 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:23 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:24 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:25 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:26 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:27 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:28 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:29 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:30 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:31 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:32 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:33 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:34 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:35 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:36 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:09:37 --> 404 Page Not Found: Public/tz.php
+ERROR - 2017-07-29 08:10:36 --> 404 Page Not Found: /index
